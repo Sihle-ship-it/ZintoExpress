7cecac2d199f916f6c317d958d5b64b0ebb43482
Sihle <innocentsihle29@gmail.com>
2022-12-02 03:05:23 +0200

    Initial Commit

---
+++ server/src/test/java/zinto/express/io/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package zinto.express.io;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
